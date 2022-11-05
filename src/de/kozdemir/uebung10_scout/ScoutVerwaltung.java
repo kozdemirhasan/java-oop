625780559a284c20ed7274596bda1fee8c642491
Hasan Murat Kücüközdemir <kozdemirhasan@gmail.com>
2022-11-05 14:36:16 +0100

    scout update

--- src/de/kozdemir/uebung10_scout/ScoutVerwaltung.java
+++ src/de/kozdemir/uebung10_scout/ScoutVerwaltung.java
@@ -1,5 +1,6 @@
 package de.kozdemir.uebung10_scout;
 
+
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -10,19 +11,20 @@ import java.util.Comparator;
 import java.util.HashSet;
 import java.util.Iterator;
 
-public class ScoutManager {
+public class ScoutVerwaltung {
 
 	private static final Scanner scanner = new Scanner(System.in);
+
 	private List<Scout> scoutsList;
 
 	public static void main(String[] args) {
 
-		ScoutManager sm = new ScoutManager();
+		ScoutVerwaltung sm = new ScoutVerwaltung();
 
 		// erste version
 //		sm.ersteVersion();
 
-		//Erweiterung
+		// Erweiterung
 		Scout s1 = new Scout("Peter", "Parker", LocalDate.of(1998, 5, 15), 202);
 		Scout s2 = new Scout("Carol", "Danvers", LocalDate.of(2000, 7, 2), 177);
 		Scout s3 = new Scout("Bruce", "Banner", LocalDate.of(1982, 12, 1), 200);
@@ -37,15 +39,13 @@ public class ScoutManager {
 		sm.addScout(s5);
 		sm.addScout(s6);
 		sm.addScout(s7);
-		
+
 		System.out.println();
-		
+
 //		Erweiterung-2
-		sm.sortierung(); //Vorname dann Nachname dann Gebustdatum dannach Größe
+		sm.sortierung(); // Vorname dann Nachname dann Gebustdatum dannach Größe
 
 		sm.printScouts();
-
-
 
 	}
 
@@ -55,15 +55,16 @@ public class ScoutManager {
 		Comparator<Scout> sortGeburstag = (scout1, scout2) -> scout1.getBirthDate().compareTo(scout2.getBirthDate());
 		Comparator<Scout> sortGroesse = (scout1, scout2) -> scout1.getHeight().compareTo(scout2.getHeight());
 
-		Collections.sort(scoutsList, sortFirstName.thenComparing(sortLastName).thenComparing(sortGeburstag).thenComparing(sortGroesse));
-		
+		Collections.sort(scoutsList,
+				sortFirstName.thenComparing(sortLastName).thenComparing(sortGeburstag).thenComparing(sortGroesse));
+
 	}
 
 	public void addScout(Scout scout) {
 		// wenn keine scoutList dann scoutList vonn HasSet erstellt
 		if (scoutsList == null)
 			scoutsList = new ArrayList<>();
-	
+
 		boolean b = widerholenPruefung(scout);
 		if (b == false)
 			scoutsList.add(scout);
@@ -91,42 +92,42 @@ public class ScoutManager {
 //			System.out.println(
 //					s.getFirstName() + " " + s.getLastName() + " " + s.getBirthDate().toString() + " " + s.getHeight());
 //		}
-				
-		System.out.println(String.format("%-15s   | %-15s  |  %-15s  |  %-10s  ", "Vorname", "Nachname", "Gebursdatum", "Größe"));
+
+		System.out.println(
+				String.format("%-15s   | %-15s  |  %-15s  |  %-10s  ", "Vorname", "Nachname", "Gebursdatum", "Größe"));
 		System.out.println("--------------------------------------------------------------------");
-		scoutsList.forEach(s -> System.out.println(String.format("%-15s   | %-15s  |  %-15s  |  %-10s    \n", s.getFirstName(),
-				s.getLastName(), s.getBirthDate().toString(), s.getHeight())));
+		scoutsList.forEach(s -> System.out.println(String.format("%-15s   | %-15s  |  %-15s  |  %-10s    \n",
+				s.getFirstName(), s.getLastName(), s.getBirthDate().toString(), s.getHeight())));
 
 	}
 
-	
-	  public void ersteVersion() { Scout s1 = new Scout("Peter", "Parker",
-	  LocalDate.of(1998, 5, 15), 182); Scout s2 = new Scout("Carol", "Danvers",
-	  LocalDate.of(2000, 7, 2), 177); Scout s3 = new Scout("Bruce", "Banner",
-	  LocalDate.of(1982, 12, 1), 200); Scout s4 = new Scout("Natasha", "Romanov",
-	  LocalDate.of(1998, 1, 3), 175); Scout s5 = new Scout("Steve", "Taylor",
-	  LocalDate.of(1987, 5, 9), 179); Scout s6 = new Scout("Natasha", "Romanov",
-	  LocalDate.of(1998, 1, 2), 175);
-	  
-	  List<Scout> scoutsList = new ArrayList<>(); scoutsList.add(s1);
-	  scoutsList.add(s2); scoutsList.add(s3); scoutsList.add(s4);
-	  scoutsList.add(s5); scoutsList.add(s6);
-	  
-	  Comparator<Scout> sortFirstName = (scout1, scout2) ->
-	  scout1.getFirstName().compareTo(scout2.getFirstName()); Comparator<Scout>
-	  sortLastName = (scout1, scout2) ->
-	  scout1.getLastName().compareTo(scout2.getLastName()); Comparator<Scout>
-	  sortGeburstag = (scout1, scout2) ->
-	  scout1.getBirthDate().compareTo(scout2.getBirthDate());
-	  
-	  Collections.sort(scoutsList,
-	  sortFirstName.thenComparing(sortLastName).thenComparing(sortGeburstag));
-	  
-	  for (Scout s : scoutsList) { System.out.println(s.getFirstName() + " " +
-	  s.getLastName() + " " + s.getBirthDate()); } }
-	 
-	
-	
+	public void ersteVersion() {
+		Scout s1 = new Scout("Peter", "Parker", LocalDate.of(1998, 5, 15), 182);
+		Scout s2 = new Scout("Carol", "Danvers", LocalDate.of(2000, 7, 2), 177);
+		Scout s3 = new Scout("Bruce", "Banner", LocalDate.of(1982, 12, 1), 200);
+		Scout s4 = new Scout("Natasha", "Romanov", LocalDate.of(1998, 1, 3), 175);
+		Scout s5 = new Scout("Steve", "Taylor", LocalDate.of(1987, 5, 9), 179);
+		Scout s6 = new Scout("Natasha", "Romanov", LocalDate.of(1998, 1, 2), 175);
+
+		List<Scout> scoutsList = new ArrayList<>();
+		scoutsList.add(s1);
+		scoutsList.add(s2);
+		scoutsList.add(s3);
+		scoutsList.add(s4);
+		scoutsList.add(s5);
+		scoutsList.add(s6);
+
+		Comparator<Scout> sortFirstName = (scout1, scout2) -> scout1.getFirstName().compareTo(scout2.getFirstName());
+		Comparator<Scout> sortLastName = (scout1, scout2) -> scout1.getLastName().compareTo(scout2.getLastName());
+		Comparator<Scout> sortGeburstag = (scout1, scout2) -> scout1.getBirthDate().compareTo(scout2.getBirthDate());
+
+		Collections.sort(scoutsList, sortFirstName.thenComparing(sortLastName).thenComparing(sortGeburstag));
+
+		for (Scout s : scoutsList) {
+			System.out.println(s.getFirstName() + " " + s.getLastName() + " " + s.getBirthDate());
+		}
+	}
+
 	public void scoutEingabe() {
 
 		while (true) {
@@ -180,6 +181,5 @@ public class ScoutManager {
 
 		}
 	}
-
 
 }
