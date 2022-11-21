435276adcd5bb3ad41a8d6595023409940fe580f
Hasan Murat Kücüközdemir <kozdemirhasan@gmail.com>
2022-11-21 08:14:16 +0100

    uebung 12 update

---
+++ src/de/kozdemir/uebung12/GruessTest.java
@@ -0,0 +1,59 @@
+package de.kozdemir.uebung12;
+
+public class GruessTest {
+
+	public static void main(String[] args) {
+
+		Begueruessen b1 = s -> System.out.println("Merhaba " + s + "!");
+		Begueruessen b2 = s -> System.out.println("Hallo " + s + "!");
+		Begueruessen b3 = s -> System.out.println("Hello " + s + "!");
+
+		b1.gruessen("Hasan");
+		b2.gruessen("Hasan");
+		b3.gruessen("Hasan");
+
+	
+
+		System.out.println("----------------------------------------");
+		
+		f(new Begueruessen() {
+			@Override
+			public void gruessen(String kimi) {
+				System.out.println("HALLO " + kimi);
+				
+			}
+		});
+		
+		System.out.println("----------------------------------------");
+
+		Begueruessen2 bn = new Begueruessen2() {
+			@Override
+			public String gruessen(String kimi) {
+				return "Merhaba " + kimi + " ***";
+			}
+		};
+		System.out.println(bn.gruessen("Peter"));
+		
+	
+		System.out.println("----------------------------------------");
+
+
+	}
+
+	public static void f(Begueruessen b) {
+		b.gruessen("Mustafa");
+	}
+
+}
+
+@FunctionalInterface
+interface Begueruessen {
+	public void gruessen(String wenn);
+}
+
+@FunctionalInterface
+interface Begueruessen2 {
+	public String gruessen(String wenn);
+}
+
+
---
+++ src/de/kozdemir/uebung12/TemperaturumrechnungenTest.java
@@ -0,0 +1,35 @@
+package de.kozdemir.uebung12;
+
+public class TemperaturumrechnungenTest {
+
+	public static void main(String[] args) {
+		
+		double eingabe = 30;
+
+		Temperaturumrechnungen celvinToFahrenheit = d -> 1.8 * (d - 273) + 32;
+		System.out.println(eingabe + " Celvin = " + celvinToFahrenheit.konvertieren(eingabe) + " Fahreneit");
+
+		Temperaturumrechnungen celvinToCelcius = d -> d - 273.15;
+		System.out.println(eingabe + " Celvin = " + celvinToCelcius.konvertieren(eingabe) + " Celcius");
+
+		Temperaturumrechnungen fahrenheittoCelvin = d -> (((d - 32) * 5) / 9) + 273.15;
+		System.out.println(eingabe + " Fahrenheit = " + fahrenheittoCelvin.konvertieren(eingabe) + " Celvin");
+
+		Temperaturumrechnungen fahrenheitToCelcius = d -> ((d - 32) * 5) / 9;
+		System.out.println(eingabe + " Fahrenheit = " + fahrenheitToCelcius.konvertieren(eingabe) + " Celcius");
+
+		Temperaturumrechnungen celciustoFahrenheit = d -> (d * 1.8) + 32;
+		System.out.println(eingabe + " Celcius = " + celciustoFahrenheit.konvertieren(eingabe) + " Fahrenheit");
+
+		Temperaturumrechnungen celciusToCelvin = d -> d + 273.15;
+		System.out.println(eingabe + " Celcius = " + celciusToCelvin.konvertieren(eingabe) + " Celvin");
+
+	}
+
+}
+
+@FunctionalInterface
+interface Temperaturumrechnungen {
+	public double konvertieren(double d);
+}
+
